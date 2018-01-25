5efa33e58841b6eb6913ff656fc468aa82eea88a
Abhishek Khanna <abhishek.khanna@abspl.in>
2018-01-25 12:42:21 +0530

    Basic Express Example

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Welcome to NodeJS Tutorial on Coursera. Tutorial Link :https://www.coursera.org/learn/server-side-nodejs/
+const express = require('express');
+const http = require('http');
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const hostname = "localhost";
+const port = "5000";
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/html');
+  res.end("<html><body> Hello, World !!</body></html>");
+});
+
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+  console.log(`running server at : ${hostname}:${port}`);
+})
