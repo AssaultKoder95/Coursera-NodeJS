ae0df51d0c160e84051a3f2f4f6191a583a1cf77
Abhishek Khanna <abhishek.khanna@abspl.in>
2018-01-25 12:55:39 +0530

    Basic REST API Example using Express

--- index.js
+++ index.js
@@ -9,10 +9,38 @@ const port = "5000";
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   res.statusCode = 200;
-  res.setHeader('Content-type', 'text/html');
+  res.setHeader('Content-Type', 'text/html');
   res.end("<html><body> Hello, World !!</body></html>");
+});
+
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+app.get('/dishes', (req, res, next) => {
+  res.end('GET operation initiated');
+});
+
+app.post('/dishes', (req, res, next) => {
+  res.end('POST operation initiated');
+});
+
+app.put('/dishes', (req, res, next) => {
+  res.end('PUT operation initiated');
+});
+
+app.delete('/dishes', (req, res, next) => {
+  res.end('DELETE operation initiated');
 });
 
 const server = http.createServer(app);
